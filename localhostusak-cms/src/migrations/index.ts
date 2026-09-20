e044d818060623325122c3688e19430ad6d3e782
localhostusak <localhostusak@gmail.com>
2026-09-20 06:43:15 +0300

    Add Payload password reset timestamp migration

--- localhostusak-cms/src/migrations/index.ts
+++ localhostusak-cms/src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260920_011155_initial_schema from './20260920_011155_initial_schema';
+import * as migration_20260920_034150_add_reset_password_requested_at from './20260920_034150_add_reset_password_requested_at';
 
 export const migrations = [
   {
     up: migration_20260920_011155_initial_schema.up,
     down: migration_20260920_011155_initial_schema.down,
-    name: '20260920_011155_initial_schema'
+    name: '20260920_011155_initial_schema',
+  },
+  {
+    up: migration_20260920_034150_add_reset_password_requested_at.up,
+    down: migration_20260920_034150_add_reset_password_requested_at.down,
+    name: '20260920_034150_add_reset_password_requested_at'
   },
 ];
